ed8cbeb37c7bce1bb9f23df577b93075de98d6b8
Bradley Stouffer <voidmaelstrom@gmail.com>
2022-06-11 08:36:39 -0600

    Activity commits

---
+++ script.py
@@ -0,0 +1,43 @@
+import pprint
+import requests
+from matplotlib import pyplot as plt
+from datetime import datetime
+
+API_URL = 'https://weather-api-node-wisc.herokuapp.com/weather/'
+city = 'seattle'
+
+r = requests.get(API_URL + city)
+response = r.json()
+
+pp = pprint.PrettyPrinter(indent=4)
+
+# pp.pprint(response)
+forecast_list = response['forecast']
+
+today = datetime.now().strftime("%b-%d-%Y")
+
+to_graph = {}
+count = 1
+for day in forecast_list:
+    current_date = int(today[4:6]) + count
+    this_day = f"{today[0:4]}{current_date}{today[6:]}"
+    count+=1 if current_date <= 31 else 1
+
+    day['wind'] = 0 if day['wind'][0] == ' ' else int(day['wind'][:2])
+
+    to_graph[this_day] = day['wind']
+
+# print('to_graph dictionary: ', to_graph)
+
+plt.xlabel('Date')
+plt.ylabel('Wind Speed km/h')
+
+# print('to_graph keys: ', to_graph.keys())
+# print('to_graph values: ', to_graph.values())
+plt.scatter(to_graph.keys(), to_graph.values())
+plt.show()
+
+# expected output should look something like:
+#   {'Aug-24-2021': 12,
+#    'Aug-25-2021': 14,  
+#    'Aug-26-2021': 0 }
